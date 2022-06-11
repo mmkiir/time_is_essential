80bff32480bd91a043645c1bb1df266b17bcdf24
Porasjeet Singh <porasjeet@gmail.com>
2022-06-11 17:22:41 +0530

    Add a required delay between sweet spot attempts

--- Watch.gd
+++ Watch.gd
@@ -15,11 +15,15 @@ func _ready():
 
 
 func _process(delta):
-	if (Input.is_action_pressed("ui_accept")
-			and $WatchHandSmall.rotation_degrees > 90 - SWEET_SPOT_ANGLE / 2
-			and $WatchHandSmall.rotation_degrees < 90 + SWEET_SPOT_ANGLE / 2):
-		$WatchHand.rotation_degrees = 0
-		$Timer.start()
+	if (Input.is_action_pressed("ui_accept")):
+
+		if ($TimerSweetSpot.is_stopped()
+				and $WatchHandSmall.rotation_degrees > 90 - SWEET_SPOT_ANGLE / 2
+				and $WatchHandSmall.rotation_degrees < 90 + SWEET_SPOT_ANGLE / 2):
+			$WatchHand.rotation_degrees = 0
+			$Timer.start()
+			
+		$TimerSweetSpot.start()
 
 	$WatchHand.rotation_degrees = fmod(
 		$WatchHand.rotation_degrees + 360 / WATCH_HAND_REVOLUTION_PERIOD * delta,
